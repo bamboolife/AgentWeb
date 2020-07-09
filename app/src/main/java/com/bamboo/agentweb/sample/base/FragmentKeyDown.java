3bb0191bc3b387d54bc23c1c1b4d9a90133ee4c7
bamboo <core_it@163.com>
2020-07-09 15:32:29 +0800

    webview

---
+++ app/src/main/java/com/bamboo/agentweb/sample/base/FragmentKeyDown.java
@@ -0,0 +1,8 @@
+package com.bamboo.agentweb.sample.base;
+
+import android.view.KeyEvent;
+
+public interface FragmentKeyDown {
+
+    boolean onFragmentKeyDown(int keyCode, KeyEvent event);
+}
---
+++ app/src/main/java/com/bamboo/agentweb/sample/common/CommonWebChromeClient.java
@@ -0,0 +1,20 @@
+package com.bamboo.agentweb.sample.common;
+
+import android.util.Log;
+import android.webkit.WebView;
+
+import com.bamboo.agentweb.WebChromeClient;
+
+
+/**
+ * @author cenxiaozhong
+ * @date 2019/2/19
+ * @since 1.0.0
+ */
+public class CommonWebChromeClient extends WebChromeClient {
+	@Override
+	public void onProgressChanged(WebView view, int newProgress) {
+		  super.onProgressChanged(view, newProgress);
+		Log.i("CommonWebChromeClient", "onProgressChanged:" + newProgress + "  view:" + view);
+	}
+}
